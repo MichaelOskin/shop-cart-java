8463af09a08328144d792e0be556fc08bceb68bf
MIchael <139218970+MichaelOskin@users.noreply.github.com>
2024-02-27 23:50:09 +0400

    Delete shop-cart-java directory

--- shop-cart-java/src/main/java/org/penzgtu/Application/Application.java
+++
@@ -1,7 +0,0 @@
-package org.penzgtu.Application;
-
-import org.penzgtu.Application.menu.AuthenticationServiceMenu;
-
-public class Application {
-    public static void main(String[] args) {AuthenticationServiceMenu.authenticationMenu();}
-}
